9aeb7ce0dea0deaed9fb5530801104790a331920
Laura Vos (Gagnon) <142829338+lauravos@users.noreply.github.com>
2024-05-24 20:37:45 -0500

    Delete import csv.py

--- import csv.py
+++
@@ -1,9 +0,0 @@
-import csv      
-
-
-  # Create a CSV reader object
-#reader = csv.reader('tasks.csv', delimiter=",") 
-
-with open('tasks.csv', 'r', newline='') as csvfile:
-    for row in csvfile:
-        print(row)
